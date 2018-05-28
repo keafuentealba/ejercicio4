404b37772bf0b11e4444b7cd766ff0813825fc81
camila fuentealba <camila.fuentealba.rubilar@gmail.com>
2018-05-28 02:36:01 -0400

    prueba

---
+++ app.js
@@ -0,0 +1,10 @@
+let print = (t) =>{
+  let contador = 0;
+  while (t.length > contador) {
+    contador++
+  }
+let texto = prompt("asd");// ingresa datos
+/*while(texto.length > contador){
+console.log(texto[contador].toUpperCase());
+contador++;
+}
